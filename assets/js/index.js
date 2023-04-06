b39c9d7427d24cc5dd6aed44cecf8ecb6e70a766
Alihaider0315 <ah712200@gmail.com>
2023-04-06 06:42:03 +0500

    Custom Responsive Web Project

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open')
+
+}
